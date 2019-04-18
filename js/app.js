0023d2b2a9032648b647baabeee395654e6c8bbc
windyludev <windyludev@outlook.com>
2019-04-18 00:35:00 -0400

    Get information from the DOM and add events to the buttons

--- js/app.js
+++ js/app.js
@@ -1,3 +1,40 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById('btn__reset');
+    
+    button.addEventListener('click', play);
+})
+
+function play() {
+    const phraseContainer = document.getElementsByTagName("ul")[0];
+    const overlay = document.getElementById('overlay');
+    const game = new Game(phraseContainer, overlay, getScoreBar());
+    
+    overlay.style.display = 'none';
+    
+    game.starGame();
+    activateKeyboard(game, overlay);
+    
+}
+
+function activateKeyboard(game) {
+    const keys = document.getElementById('qwerty');
+    keys.addEventListener('click', (e) => {
+        const selected = e.target;
+
+        if (selected.tagName !== "BUTTON") {
+            return;
+        }
+        
+        game.handleInteraction(selected, );
+    })
+
+}
+
+function getScoreBar() {
+    const container = document.getElementById('scoreboard');
+    return container.getElementsByTagName('img');
+}
+
