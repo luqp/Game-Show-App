d3dbe0ea3f135dfcffb12761b965962c12ec2bf8
windyludev <windyludev@outlook.com>
2019-04-19 12:54:36 -0400

    Organise structure and delete 'play' function

--- js/app.js
+++ js/app.js
@@ -2,39 +2,58 @@
  * Project 4 - OOP Game App
  * app.js */
 document.addEventListener("DOMContentLoaded", () => {
-    const button = document.getElementById('btn__reset');
-    
-    button.addEventListener('click', play);
-})
-
-function play() {
     const phraseContainer = document.getElementsByTagName("ul")[0];
-    const overlay = document.getElementById('overlay');
-    const game = new Game(phraseContainer, overlay, getScoreBar());
-    
-    overlay.style.display = 'none';
-    
-    game.starGame();
-    activateKeyboard(game, overlay);
-    
-}
+    const scoreBar = document.getElementById('scoreboard').getElementsByTagName('img');
+    const wrapper = document.getElementById('overlay');
+    const button = document.getElementById('btn__reset');
+    const game = new Game(phraseContainer, phrases, scoreBar, wrapper);
 
+    activateKeyboard(game);
+    
+    button.addEventListener('click', () => {
+        changeHearts(scoreBar, "images/live.svg");
+        game.overlay.style.display = 'none';
+        game.starGame();
+    });
+});
+// Change the hearts by hearts alive
+function changeHearts(scoreBar, image) {
+    for (const heart of scoreBar) {
+        heart.src = image;
+    }
+}
+// Add event listeners when you press on a key
 function activateKeyboard(game) {
     const keys = document.getElementById('qwerty');
+    let selectedKeys = [];
+
+    document.addEventListener('keyup', (e) => {
+        if (game.active && selectedKeys.indexOf(e.key) === -1) {
+            selectedKeys.push(e.key);
+
+            for (const keysrow of keys.children) {
+                for (const button of keysrow.children) {
+                    if (button.textContent === e.key) {
+                        if (game.handleInteraction(button)) {
+                            selectedKeys = [];
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+          
+    });
+
     keys.addEventListener('click', (e) => {
         const selected = e.target;
-
-        if (selected.tagName !== "BUTTON") {
-            return;
-        }
         
-        game.handleInteraction(selected, );
+        if (game.active && selected.tagName === "BUTTON") {
+            selectedKeys.push(selected.textContent);
+            
+            if (game.handleInteraction(selected)) {
+                selectedKeys = [];
+            }
+        }
     })
-
 }
-
-function getScoreBar() {
-    const container = document.getElementById('scoreboard');
-    return container.getElementsByTagName('img');
-}
-
