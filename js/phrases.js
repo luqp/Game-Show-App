8095b176fa8d90b75a506af2fb587379458d58fc
windyludev <windyludev@outlook.com>
2019-04-19 12:48:53 -0400

    New file of data phrases

---
+++ js/phrases.js
@@ -0,0 +1,19 @@
+const phrases = [
+    'somebody',
+    'discover',
+    'thank you',
+    'very useful',
+    'number four',
+    'be involved', 
+    'the neighbors',
+    'a small boy',
+    'We were proud',
+    'their greatest fear',
+    'he had a mustache',
+    'she spent so much',
+    'he was the director',
+    'they were perfectly normal',  
+    'anything strange or mysterious',
+    'they also had a secret', 
+    'They were the last people'
+];
